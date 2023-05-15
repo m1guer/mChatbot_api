93899cec49519abc958cf930ec40205c0a412616
Miguerfi <miguelpintojose00@gmail.com>
2023-05-15 14:35:59 -0300

    :construction: Working on requests.

--- mchatbot/whatsapp/models.py
+++ mchatbot/whatsapp/models.py
@@ -20,6 +20,12 @@ class Clients(models.Model):
     FORMA_DE_PAGAMENTO_ESCOLHA = [(PIX, "Pix"),
                                   (DINHEIRO, "Dinheiro"),
                                   (CARTAO, "Credito ou Débito")]
+    SENT = "snt"
+    DELIVERIED = "dvr"
+    READ = "rad"
+    STATUS_MENSANGEM_ESCOLHA = [
+        (SENT, "Enviada"), (DELIVERIED, "Entregue"), (READ, "LIDA")]
+    conversation_id = models.CharField(max_length=31, blank=True, null=True)
     numero_whatsapp = models.IntegerField(null=True, blank=True)
     nome = models.CharField(null=True, blank=True, max_length=160)
     endereco = models.CharField(null=True, blank=False, max_length=260)
@@ -28,3 +34,5 @@ class Clients(models.Model):
     pedido = models.CharField(max_length=260, null=True, blank=True)
     num_pedido = models.ForeignKey(
         Pedido, on_delete=models.CASCADE, null=True, blank=True)
+    status_mensage = models.CharField(
+        default=SENT, choices=STATUS_MENSANGEM_ESCOLHA, max_length=3)
--- mchatbot/whatsapp/viewsets/viewsets.py
+++ mchatbot/whatsapp/viewsets/viewsets.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from whatsapp.serializers.clients_serializer import ClientsSerializers
 from whatsapp.models import Clients
+import requests
+import json
 
 
 class ClientViewSet(viewsets.ViewSet):
@@ -11,13 +13,46 @@ class ClientViewSet(viewsets.ViewSet):
         serializer = ClientsSerializers(queryset, many=True)
         return Response(serializer.data)
 
+    def sent_message(self, numero, nome):
+        url = "https://graph.facebook.com/v16.0/121601520926198/messages"
+        headers = {
+            "Authorization": "Bearer EAACDHcMuPpIBAI2ZA4V5LXg62YPWh8FuAlWQSebZBbaHfMZCjui2t5U1yLEULauqJxuC5kki0OlDKxed6W7qt0AoZC8DwJRdL8dAGNgg1kpV9vhSMZALVp2IjQ0HEITjPz9Yn3GRveL70Va1EryPOjZCiAd1ZAc7tMPevc0AdyFKemSaJFb2E5dssImHVZC0H0nz2xM7pRRAuQZDZD",
+            "Content-Type": "application/json",
+        }
+        body = {
+            "messaging_product": "whatsapp",
+            "to": numero,
+            "type": "text",
+            "text": {
+                "body": "Olá " + nome
+            }
+        }
+
+        r = requests.post(url, headers=headers, data=json.dumps(body))
+        return r
+
     @action(detail=False, methods=['get', 'post'])
     def webhook(self, request, *args, **kwargs):
+        # verificação do faecbook para meu webhook
+        if request.method == 'GET':
+            hub_challenge = request.query_params.get('hub.challenge')
+            data = json.loads(hub_challenge)
+            return Response(data)
+
+        cliente = Clients()
         json_data = request.data
         name = json_data['entry'][0]['changes'][0]['value']['contacts'][0]['profile']['name']
-        numero_client = json_data['entry'][0]['changes'][0]['value']['contacts'][0]['wa_id']
-        client = Clients()
-        client.nome = name
-        client.numero_whatsapp = numero_client
-        client.save()
-        return Response({'OK'})
+        numero = json_data['entry'][0]['changes'][0]['value']['contacts'][0]["wa_id"]
+        if request.method == 'POST':
+            if 'contacts' in json_data['entry'][0]['changes'][0]['value']:
+                try:
+                    verify_numero_exists = Clients.objects.get(
+                        numero_whatsapp=numero)
+                    self.sent_message(nome=name, numero=numero)
+                    return Response({'status': 'ok'})
+                except Clients.DoesNotExist:
+                    cliente.nome = name
+                    cliente.numero_whatsapp = numero
+                    cliente.save()
+                    self.sent_message(nome=name, numero=numero)
+                    return Response({'status': 'ok'})
